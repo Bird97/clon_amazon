be42c0addaa9837744a9cb629451281399616c8c
Bird97 <juamacx@gmail.com>
2022-04-23 17:01:38 -0500

    probando source control

--- src/Header.js
+++ src/Header.js
@@ -3,21 +3,21 @@ import "./Header.css";
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import StorefrontIcon from '@mui/icons-material/Storefront';
 import SearchIcon from '@mui/icons-material/Search';
+import {Link} from "react-router-dom";
 
 function Header() { //componente Header
   return (
     <div className='header'>
-        <div className='header__logo'>
-            <StorefrontIcon className='header__logoImage' fontSize="large"/>
-        <h2 className='header__logoTitle'>eSHOP</h2>
-        </div>
-    
+        <Link to="/" style={{textDecoration: "none"}}>
+            <div className='header__logo'> 
+                <StorefrontIcon className='header__logoImage' fontSize="large"/>
+            <h2 className='header__logoTitle'>eSHOP</h2>
+            </div>
+        </Link>
         <div className="header__search">
             <input type="text" className='header__searchInput'/>
-            <SearchIcon className='header__searchIcon'/>
-
-        </div>
-
+            <SearchIcon className='header__searchIcon' fontSize="large"/>
+        </div>     
         <div className="header__nav">
             <div className="nav__item">
                 <span className="nav__itemLineOne">Hello Guest</span>
@@ -27,11 +27,13 @@ function Header() { //componente Header
                 <span className="nav__itemLineOne">Your</span>
                 <span className="nav__itemLineTwo">Shop</span>
             </div>
-            <div className="nav__itemBasket">
-                <ShoppingBasketIcon/>
-                <span className="nav__itemLineOne">Basket</span>
-                <span className="nav__itemLineTwo nav__basketCount">0</span>
-            </div>
+            <Link to="/checkout" style={{textDecoration: "none"}}>        
+                <div className="nav__itemBasket">
+                    <ShoppingBasketIcon/>
+                    <span className="nav__itemLineOne">Basket</span>
+                    <span className="nav__itemLineTwo nav__basketCount">0</span>
+                </div>
+            </Link>
         </div>
     </div>
     )
