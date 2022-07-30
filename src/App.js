85c481ad8e0ad0b294d9ea800dcbe82ea2f0dc3a
Bird97 <juamacx@gmail.com>
2022-07-30 12:51:14 -0500

    login Firebase

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import './App.css';
 import Header from "./Header"
 import Home from "./Home"
 import Checkout from './Checkout';
+import Login from './Login';
 import {BrowserRouter, Routes,Route} from "react-router-dom";
 
-function App() { //componente App
+function App() {//componente App
   return (
     <div className="App">
       <BrowserRouter>    
         <Routes>
           <Route path='/' element={<><Header /><Home /></>}/>    
           <Route path='/checkout' element={<><Header/><Checkout/></>}/>
-          <Route path='*' element={<><Header /><Home /></>}/>      
+          <Route path='*' element={<><Header /><Home /></>}/>     
+          <Route path='/login' element={<><Header /><Login /></>}/>  
         </Routes>   
       </BrowserRouter> 
     </div>
